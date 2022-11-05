aa73e948171e97c52778c26ca422f0822aa8dca8
John Turner <jturner.usa@gmail.com>
2022-11-05 02:37:46 -0400

    crate: added some top level docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,36 @@
+//! A map-like data structure that provides weak, light weight handles to inserted values.
+//! # Example use
+//! ```
+//! use slotmap::StandardSlotMap;
+//!
+//! let mut slotmap = StandardSlotMap::new();
+//!
+//! // insert a value
+//! let a = slotmap.insert("a".to_owned());
+//!
+//! // access the values via their associated keys
+//! assert_eq!(slotmap[a], "a");
+//!
+//! // mutate a value
+//! slotmap[a] = "new a".to_owned();
+//! assert_eq!(slotmap[a], "new a");
+//!
+//! // remove a value
+//! let removed = slotmap.remove(a).unwrap();
+//! assert_eq!(removed.as_str(), "new a");
+//!
+//! // using a stale key doesn't work
+//! assert!(matches!(slotmap.get(a), None));
+//! ```
+
 #![deny(clippy::pedantic)]
 
 #[allow(dead_code, unused_variables)]
 pub mod indirection;
 mod macros;
 pub mod standard;
+
+pub use standard::SlotMap as StandardSlotMap;
 
 type IterItem<'a, T> = (Key, &'a T);
 type IterMutItem<'a, T> = (Key, &'a mut T);
